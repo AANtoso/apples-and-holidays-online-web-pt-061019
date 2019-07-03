1aefdccaf377169d72aac6a1143271fe4d3a4672
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-03 17:24:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.map {|seasons, holidays|
-    holidays.map {|holiday, supply|}
+    holidays.map {|holiday, supply|
       if supply.include?("BBQ")
         holiday
       end
